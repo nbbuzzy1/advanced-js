90e284f70ee57f7079304fac420fca9219438f54
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-12-17 14:48:16 -0500

    Class and prototype.

---
+++ class.js
@@ -0,0 +1,27 @@
+class Particle {
+	constructor(x, y, square) {
+		this.x = x;
+		this.y = y;
+	}
+
+	update() {
+		this.x += 5;
+	}
+}
+
+class Confetti extends Particle {
+	constructor(x, y) {
+		super(x, y);
+	}
+
+	bright() {
+		return this.y;
+	}
+
+	update() {
+		return this.x + this.y;
+	}
+}
+
+const conf = new Confetti(2, 4);
+console.log(conf);
--- prototype.js
+++ prototype.js
@@ -1,6 +1,47 @@
-const Car = function (make) {
+const car = function (make) {
 	this.make = make;
+	function test(a) {
+		return a;
+	}
+
+	this.test2 = function (a) {
+		return a;
+	}
 }
 
-const mustang = new Car('mustang');
+const mustang = new car('mustang');
 console.log(mustang);
+// console.log(mustang.test('asdf')) //this returns a TypeError
+console.log(mustang.test2('asdf'))
+
+/////////YouTube Video
+
+function Particle() {
+	this.x = 100;
+	this.y = 99;
+}
+
+Particle.prototype.show = function () {
+	return `${this.x}, ${this.y}`
+}
+
+const p = new Particle();
+
+Array.prototype.hi = function () {
+	console.log('hi');
+}
+
+const test = [1, 2, 4];
+test.hi()
+
+const NewParticle = function () {
+	Particle.call(this);
+	this.new = 'new'
+}
+
+NewParticle.prototype = Object.create(Particle.prototype);
+NewParticle.prototype.constructor = NewParticle;
+
+const newtest = new NewParticle();
+console.log(newtest);
+newtest.show();
